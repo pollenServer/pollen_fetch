fa1812f82d2d90c1e108f2e57b96d185785286e4
Wooseobee <dntjq0807@naver.com>
2023-09-06 08:52:59 +0900

    init pollen_fetch

---
+++ src/main/java/pollen/pollen_fetch/PollenFetchApplication.java
@@ -0,0 +1,13 @@
+package pollen.pollen_fetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollenFetchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollenFetchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pollen/pollen_fetch/PollenFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package pollen.pollen_fetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollenFetchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
