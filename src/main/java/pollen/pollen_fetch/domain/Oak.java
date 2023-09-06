d3271aa18aeda7a212e2d418d0727d5737f46ab2
Wooseobee <dntjq0807@naver.com>
2023-09-06 12:55:21 +0900

    feat: add domains

---
+++ src/main/java/pollen/pollen_fetch/domain/Oak.java
@@ -0,0 +1,21 @@
+package pollen.pollen_fetch.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Oak {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private String areaCode; 
+
+    private int today;
+
+    private int tomorrow;
+
+    private int dayaftertomorrow;
+}
---
+++ src/main/java/pollen/pollen_fetch/domain/Weeds.java
@@ -0,0 +1,21 @@
+package pollen.pollen_fetch.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Weeds {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String areaCode;
+    
+    private int today;
+
+    private int tomorrow;
+
+    private int dayaftertomorrow;
+}
