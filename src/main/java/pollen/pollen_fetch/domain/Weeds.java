9e78941d13bb0d55fb779943a019d5c0ed91106a
Wooseobee <dntjq0807@naver.com>
2023-09-07 20:40:33 +0900

    develop: update Domains

--- src/main/java/pollen/pollen_fetch/domain/Weeds.java
+++ src/main/java/pollen/pollen_fetch/domain/Weeds.java
@@ -1,10 +1,13 @@
 package pollen.pollen_fetch.domain;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
 public class Weeds {
 
     @Id
@@ -18,4 +21,11 @@ public class Weeds {
     private int tomorrow;
 
     private int dayaftertomorrow;
+
+    public Weeds(String areaCode, int today, int tomorrow, int dayaftertomorrow) {
+        this.areaCode = areaCode;
+        this.today = today;
+        this.tomorrow = tomorrow;
+        this.dayaftertomorrow = dayaftertomorrow;
+    }
 }
