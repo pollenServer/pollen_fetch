dca27861a340ef56b1377ab5b6849eddc9cf969c
Wooseobee <dntjq0807@naver.com>
2023-09-08 08:57:28 +0900

    develop: Repositories areaNo로 검색 기능 추가

--- src/main/java/pollen/pollen_fetch/repository/OakRepository.java
+++ src/main/java/pollen/pollen_fetch/repository/OakRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pollen.pollen_fetch.domain.Oak;
 
 public interface OakRepository extends JpaRepository<Oak, Long> {
+    public Long findByAreaNo(String areaNo);
 }
