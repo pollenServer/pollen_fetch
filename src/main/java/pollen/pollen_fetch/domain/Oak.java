c50e2c49b8969190afe8d076e8b2e31ccd161dc8
Wooseobee <dntjq0807@naver.com>
2023-09-12 21:14:49 +0900

    develop: update domains

--- src/main/java/pollen/pollen_fetch/domain/Oak.java
+++ src/main/java/pollen/pollen_fetch/domain/Oak.java
@@ -3,6 +3,7 @@ package pollen.pollen_fetch.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -20,11 +21,18 @@ public class Oak {
     
     private String areaNo;
 
+    @Nullable
     private int today;
 
+    @Nullable
     private int tomorrow;
 
+    @Nullable
     private int dayaftertomorrow;
+
+    public Oak(String areaNo) {
+        this.areaNo = areaNo;
+    }
 
     public Oak(String areaNo, int today, int tomorrow, int dayaftertomorrow) {
         this.areaNo = areaNo;
--- src/main/java/pollen/pollen_fetch/domain/Pine.java
+++ src/main/java/pollen/pollen_fetch/domain/Pine.java
@@ -3,6 +3,7 @@ package pollen.pollen_fetch.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,12 +20,19 @@ public class Pine {
     private Long id;
 
     private String areaNo;
-    
+
+    @Nullable
     private int today;
 
+    @Nullable
     private int tomorrow;
 
+    @Nullable
     private int dayaftertomorrow;
+
+    public Pine(String areaNo) {
+        this.areaNo = areaNo;
+    }
 
     public Pine(String areaNo, int today, int tomorrow, int dayaftertomorrow) {
         this.areaNo = areaNo;
