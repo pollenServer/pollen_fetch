3a487f2a18095b8b9fb92bb6fe2ed051b6c3fab5
Wooseobee <dntjq0807@naver.com>
2023-09-13 22:30:39 +0900

    fix: remove tests

--- src/test/java/pollen/pollen_fetch/PollenFetchApplicationTests.java
+++ src/test/java/pollen/pollen_fetch/PollenFetchApplicationTests.java
@@ -3,7 +3,7 @@ package pollen.pollen_fetch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PollenFetchApplicationTests {
 
     @Test
